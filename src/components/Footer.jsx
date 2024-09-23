193ff0d7a87ffee01cbc7b8b262667a16ef567c2
Shubham Yadav <shubhamyadav97660@gmail.com>
2024-09-23 18:03:12 +0530

    added rel to anchor tags as 'noopener noreferrer' which provides security by preventing the new page from accessing window.opener

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,23 @@ export const Footer = () => {
         href="https://www.linkedin.com/in/shubham-yadav-6676a7239/"
         target="_blank"
         className="text-sky-500"
+        rel="noopener noreferrer"
       >
         Linkedin
       </a>
       <p>|</p>
-      <a href="https://github.com/shubhamgyd" target="_blank">
+      <a
+        href="https://github.com/shubhamgyd"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Github
       </a>
       <p>|</p>
       <a
         href="https://github.com/shubhamgyd/NotesReactApp"
         target="_blank"
+        rel="noopener noreferrer"
         className="text-teal-400"
       >
         Source Code
